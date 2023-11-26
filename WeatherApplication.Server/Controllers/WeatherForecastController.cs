75b45f1da05ff57cf94974950d72ce88e71fa629
Alexander Ryabishchuk <ar@a2software.dk>
2023-11-26 21:39:38 +0200

    [add] method with comments

--- WeatherApplication.Server/Controllers/WeatherForecastController.cs
+++ WeatherApplication.Server/Controllers/WeatherForecastController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json; // You need to install Newtonsoft.Json nugget package 
 using System.ComponentModel.DataAnnotations;
+using System.Text;
 using WeatherApplication.Server.DTOs;
 
 namespace WeatherApplication.Server.Controllers
@@ -10,6 +12,7 @@ namespace WeatherApplication.Server.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly OpenWeather _openWeather;
+        private readonly HttpClient _httpClient;
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,8 +21,10 @@ namespace WeatherApplication.Server.Controllers
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger,
-            IOptions<OpenWeather> openWeather)
+            IOptions<OpenWeather> openWeather,
+            HttpClient httpClient)
         {
+            _httpClient = httpClient; // Use DI to get HTTPClient correctly
             _openWeather = openWeather.Value;
             _logger = logger;
         }
@@ -37,11 +42,60 @@ namespace WeatherApplication.Server.Controllers
         }
 
         [HttpGet("CurrentWeather")]
-        public CurrentWeatherDto GetCurrentWeather([FromQuery][Required] string cityName,
-                                                   [FromQuery] string stateCode,
+        public async Task<ActionResult<CurrentWeatherDto>> GetCurrentWeather([FromQuery][Required] string cityName, // Add one more decorator Task<>
+                                                                                                                    // To handle many asynchronous methods
+                                                   [FromQuery] int? stateCode,
                                                    [FromQuery] int? countryCode)
         {
-            return new CurrentWeatherDto();
+            // Check if we got required data at all
+            if(_openWeather == null || string.IsNullOrWhiteSpace(cityName))
+            {
+                return BadRequest("Some configuration or request is empty"); // Return bad request with message that points to problem
+            }
+
+            // Use stringbuilder to build url for geocode
+            StringBuilder geocode = new StringBuilder();
+            string geocodeUrl = geocode.Append(_openWeather.Site + _openWeather.GeoResponseType + _openWeather.GeoVersion)
+                      .Append(_openWeather.GeolocationTemplate.Replace("cityname", cityName)
+                      .Replace(",stateCode", stateCode.HasValue ? stateCode.Value.ToString() : "")
+                      .Replace(",countrycode", countryCode.HasValue ? countryCode.Value.ToString() : "")
+                      .Replace("APIKey", _openWeather.Key)).ToString();
+
+            var geoResponse = await _httpClient.GetAsync(geocodeUrl); // Make asynchronous call to Open Weather site
+
+            if(!geoResponse.IsSuccessStatusCode || geoResponse == null || geoResponse.Content == null)
+            {
+                return BadRequest("Call to Open Weather for geocode failed");
+            }
+
+            string geo = await geoResponse.Content.ReadAsStringAsync(); // Transform response to string
+            var geoCode = JsonConvert.DeserializeObject<GeoCodeDto>(geo);
+
+            if(geoCode == null)
+            {
+                return BadRequest("Deserialization of geocode failed");
+            }
+
+            // if previous actions are successful - create url for current weather
+            StringBuilder currentWeatherUrl = new StringBuilder();
+            currentWeatherUrl.Append(_openWeather.Site + _openWeather.WeatherResponseType + _openWeather.WeatherVersion)
+                             .Append(_openWeather.CurrentWeatherTemplate.Replace("=lat", "=" + geoCode.Lat)
+                             .Replace("=lon", "=" + geoCode.Lon).Replace("APIKey", _openWeather.Key)).ToString();
+
+            var currentWeatherResponse = await _httpClient.GetAsync(geocodeUrl); // Make asynchronous call to Open Weather site
+            if (!currentWeatherResponse.IsSuccessStatusCode || currentWeatherResponse == null || currentWeatherResponse.Content == null)
+            {
+                return BadRequest("Call to Open Weather for current weather failed");
+            }
+
+            string current = await geoResponse.Content.ReadAsStringAsync(); // Transform response to string
+            var currentWeather = JsonConvert.DeserializeObject<CurrentWeatherDto>(current);
+            if (currentWeather == null)
+            {
+                return BadRequest("Deserialization of current weather failed");
+            }
+
+            return Ok(currentWeather);
         }
     }
 }
--- WeatherApplication.Server/Program.cs
+++ WeatherApplication.Server/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddTransient<HttpClient>(); // this is kind of DI, but used in no production way
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
