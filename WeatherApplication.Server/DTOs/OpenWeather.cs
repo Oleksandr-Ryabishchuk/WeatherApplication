3e609dec745d558df40daa4e07d64bc44f832068
Alexander Ryabishchuk <ar@a2software.dk>
2023-11-26 18:22:09 +0200

    [add] config transmitter

---
+++ WeatherApplication.Server/DTOs/OpenWeather.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherApplication.Server.DTOs
+{
+    public class OpenWeather
+    {
+        public string Site { get; set; } = string.Empty;
+        public string GeoVersion { get; set; } = string.Empty;
+        public string WeatherVersion { get; set; } = string.Empty;
+        public string GeoResponseType { get; set; } = string.Empty;
+        public string WeatherResponseType { get; set; } = string.Empty;
+        public string CurrentWeatherTemplate { get; set; } = string.Empty;
+        public string FourDaysForecastTemplate { get; set; } = string.Empty;
+        public string Key { get; set; } = string.Empty;
+    }
+}
