b3ffd738c16c87024039d7fb3892370d001e8f86
Alexander Ryabishchuk <ar@a2software.dk>
2023-11-26 19:44:41 +0200

    [add] dtos for parsing responses

---
+++ WeatherApplication.Server/DTOs/CurrentWeatherDto.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace WeatherApplication.Server.DTOs
+{
+    public class CurrentWeatherDto
+    {
+        public Main? Main { get; set; }
+        public Wind? Wind { get; set; }
+        public Clouds? Clouds { get; set; }
+        public double Dt { get; set; } // For date
+    }
+    public class Main
+    {
+        public int Temp { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+    }
+    public class Wind
+    {
+        public double Speed { get; set; }
+    }
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+}
---
+++ WeatherApplication.Server/DTOs/GeoCodeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherApplication.Server.DTOs
+{
+    public class GeoCodeDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Lat { get; set; } = 0;
+        public double Lon { get; set; } = 0;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+    }
+}
