85f42eefb2a4e090d72071c8bf94d369205e6fcb
Alexander Ryabishchuk <ar@a2software.dk>
2023-12-19 22:21:29 +0200

    [add] dbsets as fields of ApplicationDbContext

--- WeatherApplication.Server/Data/ApplicationDbContext.cs
+++ WeatherApplication.Server/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WeatherApplication.Server.Models;
 
 namespace WeatherApplication.Server.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        DbSet<Record> Records { get; set; }
+        public DbSet<CurrentWeather> CurrentWeatherCalls { get; set; }
+        public DbSet<FiveDaysWeather> FiveDaysWeatherCalls { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
