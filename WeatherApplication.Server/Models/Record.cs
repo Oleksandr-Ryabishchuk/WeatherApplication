a5d15a9175c8f2bdb54955890e6d05d336f9ad3a
Alexander Ryabishchuk <ar@a2software.dk>
2023-12-19 21:10:11 +0200

    [add] other fields to record class + connection one to one on CurrentWeather and FiveDaysWeather

--- WeatherApplication.Server/Models/Record.cs
+++ WeatherApplication.Server/Models/Record.cs
@@ -7,5 +7,12 @@ namespace WeatherApplication.Server.Models
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
+        public string City { get; set; } = string.Empty;
+        public double Lat { get; set; } = 0;
+        public double Lon { get; set; } = 0;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public CurrentWeather? CurrentWeather { get; set; }
+        public FiveDaysWeather? FiveDaysWeather { get; set; }
     }
 }
