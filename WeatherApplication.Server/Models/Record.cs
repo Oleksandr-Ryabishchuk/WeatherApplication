c4da192cf5106e5d5b397fcccb1c2732924de830
Alexander Ryabishchuk <ar@a2software.dk>
2023-12-19 20:39:57 +0200

    [add] first model

---
+++ WeatherApplication.Server/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿using WeatherApplication.Server.Interfaces;
+
+namespace WeatherApplication.Server.Models
+{
+    public class Record
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
