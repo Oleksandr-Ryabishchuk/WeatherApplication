41a0e6de8f16d13dbdd3bd4c6586b2cf4141e40c
Alexander Ryabishchuk <ar@a2software.dk>
2023-12-19 22:15:06 +0200

    [inherit] DbContext, add constructor,  and override main DbContext for Fluent Api

--- WeatherApplication.Server/Data/ApplicationDbContext.cs
+++ WeatherApplication.Server/Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace WeatherApplication.Server.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeatherApplication.Server.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
