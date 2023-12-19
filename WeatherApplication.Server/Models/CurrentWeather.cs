6794941c0a915f376acc14f75d015e3135407354
Alexander Ryabishchuk <ar@a2software.dk>
2023-12-19 21:04:25 +0200

    [add] other classes and bind them to interfaces

---
+++ WeatherApplication.Server/Models/CurrentWeather.cs
@@ -0,0 +1,11 @@
+﻿using WeatherApplication.Server.Interfaces;
+
+namespace WeatherApplication.Server.Models
+{
+    public class CurrentWeather : IHasGuidId, IHasDateStamp, IHasSoftDelete
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ WeatherApplication.Server/Models/FiveDaysWeather.cs
@@ -0,0 +1,11 @@
+﻿using WeatherApplication.Server.Interfaces;
+
+namespace WeatherApplication.Server.Models
+{
+    public class FiveDaysWeather : IHasGuidId, IHasDateStamp, IHasSoftDelete
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
