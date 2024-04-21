d8c652278bbf12ff6596da3daa7f7b4c7521d115
OkolJr <ibrahim.060308@gmail.com>
2024-04-21 19:53:36 +0300

    [add] new routes

--- weatherapplication.client/src/app/routing.module.ts
+++ weatherapplication.client/src/app/routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RandomWeatherComponent } from "./core/pages/random-weather/random-weather.component";
+import { CurrentWeatherPageComponent } from "./core/pages/current-weather-page/current-weather-page.component";
+import { FivedaysWeatherPageComponent } from "./core/pages/fivedays-weather-page/fivedays-weather-page.component";
+import { StatisticsPageComponent } from "./core/pages/statistics-page/statistics-page.component";
 
 const routes: Routes = [
     { path: 'randomweather', component: RandomWeatherComponent },
+    { path: 'currentweather', component: CurrentWeatherPageComponent },
+    { path: 'fivedaysweather', component: FivedaysWeatherPageComponent },
+    { path: 'statistics', component: StatisticsPageComponent },
     { path: '', redirectTo: 'randomweather', pathMatch: 'full' }
   ];
   
