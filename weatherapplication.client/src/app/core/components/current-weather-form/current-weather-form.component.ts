70e0a61b2adcd7df16a2fcef6ea89b3d7c94fcc7
OkolJr <ibrahim.060308@gmail.com>
2024-04-27 00:03:58 +0300

    [add] form for getCurrentWeather

---
+++ weatherapplication.client/src/app/core/components/current-weather-form/current-weather-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather-form',
+  templateUrl: './current-weather-form.component.html',
+  styleUrls: ['./current-weather-form.component.css']
+})
+export class CurrentWeatherFormComponent {
+
+}
--- weatherapplication.client/src/app/core/pages/current-weather-page/current-weather-page.component.ts
+++ weatherapplication.client/src/app/core/pages/current-weather-page/current-weather-page.component.ts
@@ -9,9 +9,13 @@ import { CurrentWeather, HttpService } from 'src/app/services/http.service';
 export class CurrentWeatherPageComponent implements OnInit {
   currentWeather: CurrentWeather | null = null;
   constructor(private http: HttpService) {}
-
+  
+  cityName = <HTMLSelectElement>document.getElementById('city');
+  email = <HTMLSelectElement>document.getElementById('mail');
+  stateCode = <HTMLSelectElement>document.getElementById('state');
+  countryCode = <HTMLSelectElement>document.getElementById('country');
   ngOnInit() {
-   this.http.getCurrentWeather('Lviv',"albert@gmail.com",null,null).subscribe(x => this.currentWeather = x) ;
+   this.http.getCurrentWeather(this.cityName.value,this.email.value,Number(this.stateCode.value),Number(this.countryCode.value)).subscribe(x => this.currentWeather = x) ;
   }
 
 }
