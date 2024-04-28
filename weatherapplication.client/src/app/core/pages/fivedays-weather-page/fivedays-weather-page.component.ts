342a34c36e9c689de14c003ccb9bdd5b9cc87729
OkolJr <ibrahim.060308@gmail.com>
2024-04-28 19:54:30 +0300

    [add] getFiveDaysWeather and [used] DI on page five-days-weather-page

--- weatherapplication.client/src/app/core/pages/fivedays-weather-page/fivedays-weather-page.component.ts
+++ weatherapplication.client/src/app/core/pages/fivedays-weather-page/fivedays-weather-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrentWeatherQuery, FiveDaysWeather, HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-fivedays-weather-page',
   templateUrl: './fivedays-weather-page.component.html',
   styleUrls: ['./fivedays-weather-page.component.css']
 })
-export class FivedaysWeatherPageComponent {
+export class FivedaysWeatherPageComponent implements OnInit{
+  fiveDaysWeather: FiveDaysWeather | null = null;
+  constructor(private http: HttpService) {}
 
+  ngOnInit() {}
+  getFiveDaysWeather(query: Partial<CurrentWeatherQuery>) {
+    if (query) {
+      this.http
+        .getFiveDaysWeather(
+          query.city!,
+          query.email!,
+          query.cityCode!,
+          query.stateCode!
+        )
+        .subscribe((x) => (this.fiveDaysWeather = x));
+    }
+  }
 }
--- weatherapplication.client/src/app/services/http.service.ts
+++ weatherapplication.client/src/app/services/http.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FivedaysWeatherPageComponent } from '../core/pages/fivedays-weather-page/fivedays-weather-page.component';
 
 export interface WeatherForecast {
   date: string;
@@ -13,6 +14,38 @@ export interface CurrentWeather {
   humidity: number;
   windSpeed: number;
   cloudsAll: number;
+}
+export interface FiveDaysWeather {
+  Dt: number;
+  Temp: number;
+  FeelsLike: number;
+  TempMin: number;
+  TempMax: number;
+  Pressure: number;
+  SeaLevel: number;
+  GroundLevel: number;
+  Humidity: number;
+  MinMaxTempDiff: number;
+  WeatherMain: string;
+  WeatherDescription: string;
+  WeatherIcon: string;
+  Visibility: number;
+  Pop: number;
+  Rain: string | null;
+  Snow: string | null;
+  DateText: string;
+  CloudsAll: number;
+  WindSpeed: number;
+  WindDeg: number;
+  WindGust: number;
+  CityName: string;
+  Lat: number;
+  Lon: number;
+  Country: string;
+  Population: number;
+  Timezone: number;
+  Sunrise: number;
+  Sunset: number;
 }
 export interface CurrentWeatherQuery {
   city: string;
@@ -41,4 +74,16 @@ export class HttpService {
       }&countryCode=${countryCode || ''}`
     );
   }
-}
+    getFiveDaysWeather(
+      cityName: string,
+      userEmail: string,
+      stateCode: number | null,
+      countryCode: number | null
+    ) {
+      return this.http.get<FiveDaysWeather>(
+        `/weatherforecast/FiveDaysWeather?cityName=${cityName}&userEmail=${userEmail}&stateCode=${
+          stateCode || ''
+        }&countryCode=${countryCode || ''}`
+      );
+    }
+  } 
